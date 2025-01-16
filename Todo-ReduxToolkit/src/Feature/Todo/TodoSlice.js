7d756cfbaaa58490c26ac4a62df5c6059a458663
SutharMahendra <omajimahendra@gmail.com>
2025-01-16 07:35:35 +0530

    created multiple slices

---
+++ Todo-ReduxToolkit/src/Feature/Todo/TodoSlice.js
@@ -0,0 +1,20 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    todos: [{ id: 1, text: 'hello' }]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+
+        },
+        removeTodo: (state, action) => {
+
+        }
+
+    }
+})
